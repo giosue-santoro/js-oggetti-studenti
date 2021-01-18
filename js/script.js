863de3ba946e4bcac7495f6c5cc05c37c7ad7cdb
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-18 16:32:23 +0100

    aggiunti user prompt nuovo studente oggetto da pushare nell'array

--- js/script.js
+++ js/script.js
@@ -38,8 +38,24 @@ var studenti = [studente, studenteX, studenteY, studenteZ]
 //Stampo nome e cognome degli studenti nell'array
 console.log('NOME E COGNOME DEL RESTO DEGLI STUDENTI');
 
-for ( var i = 0; studenti.length > i; i++) {
+for (var i = 0; studenti.length > i; i++) {
     var singoloOggetto = studenti[i];
     console.log(singoloOggetto.nome);
     console.log(singoloOggetto.cognome);    
 }
+
+//Interazione con l'utente
+var studenteUser = {};
+
+studenteUser.nome = prompt('inserisci il nome del nuovo studente');
+studenteUser.cognome = prompt('inserisci il cognome del nuovo studente');
+studenteUser.età = prompt('inserisci l\'età del nuovo studente');
+
+//Pusho nuovo oggetto creato dall'utente all'interno dell'array già esistente
+studenti.push(studenteUser);
+console.log('Queste sono le generalità del nuovo studente:');
+
+for (var key in studenteUser) {
+    console.log(studenteUser[key])
+}
+
